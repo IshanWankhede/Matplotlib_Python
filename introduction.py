2af4d59ee58ac06c6556b1414667aab8e0dd7735
IshanWankhede <ishan.wankhede7@gmail.com>
2026-02-23 14:29:11 +0530

    Introduction to matplotlib

---
+++ introduction.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# print(matplotlib.__version__)
+
+x = np.array([2023, 2024, 2025, 2026])
+y = np.array([50, 25, 30, 20])
+
+plt.plot(x, y)
+
+plt.show()
